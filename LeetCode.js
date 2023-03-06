9c0e0ce922df3e07d2081551ec76d709d37a8157
Nhat Hoang <johnhoanghtx@gmail.com>
2023-03-06 12:37:59 -0600

    Origin

---
+++ LeetCode.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+//  Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+//  Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+
+// Input: p = [1,2], q = [1,null,2]
+// Output: false
+
+// Input: p = [1,2,1], q = [1,1,2]
+// Output: false
+
+var isSameTree = function(p, q) {
+    
+};
